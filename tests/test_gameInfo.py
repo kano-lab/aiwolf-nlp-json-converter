3dd05fd3342abe27a8771e96d0757d8390ab9ead
navleorange <navleorange.0718@gmail.com>
2024-11-24 19:45:19 +0900

    add: info test

---
+++ tests/test_gameInfo.py
@@ -0,0 +1,9 @@
+from aiwolf_nlp_common.protocol import CommunicationProtocol
+from aiwolf_nlp_json_converter.gameInfo import gameInfoConverter
+
+def test_get_game_info_dict(initialize_str, initialize_json) -> None:
+    protocol = CommunicationProtocol.initialize_from_json(received_str=initialize_str)
+    test_result = gameInfoConverter.get_game_info_dict(protocol=protocol)
+
+    assert type(test_result) is not None
+    assert test_result["day"] == initialize_json["day"]
