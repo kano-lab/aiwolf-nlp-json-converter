2edafca6b4531a1bc692c38f8991022c6082966d
navleorange <navleorange.0718@gmail.com>
2024-11-24 19:52:15 +0900

    add: type check and day check

--- tests/test_gameInfo.py
+++ tests/test_gameInfo.py
@@ -6,4 +6,5 @@ def test_get_game_info_dict(initialize_str, initialize_json) -> None:
     test_result = gameInfoConverter.get_game_info_dict(protocol=protocol)
 
     assert type(test_result) is not None
-    assert test_result["day"] == initialize_json["day"]
+    assert type(test_result) is dict
+    assert test_result["day"] == initialize_json["info"]["day"]
