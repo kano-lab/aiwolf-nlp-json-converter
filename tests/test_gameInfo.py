a7d083b5c42ceafac7d92ff33eaf29f8f76a1160
navleorange <navleorange.0718@gmail.com>
2024-11-24 19:55:32 +0900

    add: map test

--- tests/test_gameInfo.py
+++ tests/test_gameInfo.py
@@ -8,3 +8,7 @@ def test_get_game_info_dict(initialize_str, initialize_json) -> None:
     assert type(test_result) is not None
     assert type(test_result) is dict
     assert test_result["day"] == initialize_json["info"]["day"]
+    assert test_result["agent"] == initialize_json["info"]["agent"]
+    assert test_result["attackVoteList"] == []
+    assert test_result["statusMap"] == initialize_json["info"]["statusMap"]
+    assert test_result["roleMap"] == initialize_json["info"]["roleMap"]
