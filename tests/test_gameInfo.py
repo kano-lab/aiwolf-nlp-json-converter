db4560511b8b2d3983ce207980a09eb95234f5d2
navleorange <navleorange.0718@gmail.com>
2024-12-01 17:33:13 +0900

    fix: CommunicationProtocol -> Packet

--- tests/test_gameInfo.py
+++ tests/test_gameInfo.py
@@ -1,8 +1,9 @@
-from aiwolf_nlp_common.protocol import CommunicationProtocol
+import json
+from aiwolf_nlp_common.protocol import Packet
 from aiwolf_nlp_json_converter.gameInfo import gameInfoConverter
 
 def test_get_game_info_dict(initialize_str, initialize_json) -> None:
-    protocol = CommunicationProtocol.initialize_from_json(received_str=initialize_str)
+    protocol = Packet(value=json.loads(initialize_str))
     test_result = gameInfoConverter.get_game_info_dict(protocol=protocol)
 
     assert type(test_result) is not None
--- tests/test_gameSetting.py
+++ tests/test_gameSetting.py
@@ -1,8 +1,9 @@
-from aiwolf_nlp_common.protocol import CommunicationProtocol
+import json
+from aiwolf_nlp_common.protocol import Packet
 from aiwolf_nlp_json_converter.gameSetting import gameSettingConverter
 
 def test_get_game_setting_dict(initialize_str, initialize_json) -> None:
-    protocol = CommunicationProtocol.initialize_from_json(received_str=initialize_str)
+    protocol = Packet(value=json.loads(initialize_str))
     test_result = gameSettingConverter.get_game_setting_dict(protocol=protocol)
 
     assert test_result["responseTimeout"] * 1000 == initialize_json["setting"]["responseTimeout"]
